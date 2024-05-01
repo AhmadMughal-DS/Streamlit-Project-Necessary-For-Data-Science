3b55cea5a3b8d58c6ffb910650562c74f723e434
Ahmad Mughal <ahmadzafar392@gmail.com>
2024-05-02 02:20:51 +0500

    Final Product is coming

---
+++ first.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.text('My first app')
+st.text("I love Data Science")
+st.text("I am learning Streamlit")
+st.text("I am learning Data Science")
+st.text("This is mmy first app on streamlit")
+st.write("Here's our first attempt at using data to create a table:")
+st.write(12345)
